fcdcb15796708f5d28c52da50fbd520168a65152
vSharma84 <vs84@students.uwf.edu>
2026-02-20 13:07:43 -0600

    Create Event.h

---
+++ Event.h
@@ -0,0 +1,26 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+class Customer;
+
+enum EventType {
+    ARRIVAL,
+    DEPARTURE
+};
+
+class Event {
+public:
+    double eventTime;
+    EventType type;
+    Customer* cust;
+
+    Event() {}
+
+    Event(double t, EventType tp, Customer* c) {
+        eventTime = t;
+        type = tp;
+        cust = c;
+    }
+};
+
+#endif
